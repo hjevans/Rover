e4e018659e19644eb9171fefc6dbe8bafa94c1af
hjevans <hjevans@gmail.com>
2019-04-13 15:54:48 -0400

    removed delay in potentiometer readings post-exception

--- HERO Serial Example4/HERO Serial Example4/Program.cs
+++ HERO Serial Example4/HERO Serial Example4/Program.cs
@@ -22,6 +22,7 @@ namespace HERO_Serial_Example4
         static int _txIn = 0;
         static int _txOut = 0;
         static int _txCnt = 0;
+        static bool newData;
         /** Cache for reading out bytes in serial driver. */
         static byte[] _rx = new byte[1024];
         /* initial message to send to the terminal */
@@ -70,6 +71,7 @@ namespace HERO_Serial_Example4
              *   
              */
             _uart = new System.IO.Ports.SerialPort(CTRE.HERO.IO.Port1.UART, 115200);
+            //_uart = new System.IO.Ports.SerialPort(CTRE.HERO.IO.Port1.UART, 9600);
             _uart.Open();
             /* send a message to the terminal for the user to see */
             //_uart.Write(_helloMsg, 0, _helloMsg.Length);
@@ -107,21 +109,63 @@ namespace HERO_Serial_Example4
                 string[] stringReadings;
                 //string[] strCopy;
                 int[] intReadings;
-                if (_rx != null && _uart.BytesToRead > 0)
-                {
+                //if (_rx != null && _uart.BytesToRead > 0)
+                //{
                     Debug.Print("getting initial string readings");
                     stringReadings = getStrReadings();
                     //int[] intReadings;
 
                     //if (stringReadings[0] == "E" && stringReadings[3] == ";")
                     //{
+                    if (newData == true)
+                    {
                         intReadings = getIntReadings(stringReadings);
+                    
+                    // intReadings = getIntReadings(stringReadings);
                     //}
+
                     //else
                     //{
-                      //  strCopy = stringReadings;
-                        //stringReadings[0] = strCopy[2][1].ToString();
-                        //Debug.Print("New index 0 " + stringReadings[0]);
+                    //  strCopy = stringReadings;
+                    //stringReadings[0] = strCopy[2][1].ToString();
+                    //Debug.Print("New index 0 " + stringReadings[0]);
+                    //stringReadings[1] = strCopy[3] + strCopy[0];
+                    //Debug.Print("New index 1 " + stringReadings[1]);
+                    //stringReadings[2] = strCopy[1];
+                    //Debug.Print("New index 2 " + stringReadings[2]);
+                    //stringReadings[3] = strCopy[2][0].ToString();
+                    //Debug.Print("New index 3 " + stringReadings[3]);
+
+                    //intReadings = getIntReadings(stringReadings);
+                    //}
+
+
+                    if (intReadings[0] < 90)
+                    //while (true)
+                    {
+                        //set motor to run forward until specified angle
+                        motor0.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, .25);
+
+                        //_uart.Read(_rx, 0, 13);
+                        //Debug.Print("BYTEs TO READ = " + _uart.BytesToRead.ToString());
+                        //System.Threading.Thread.Sleep(10);
+                        //if (_rx != null)
+                        //{
+                          //  stringReadings = getStrReadings();
+                        //}
+                        //if (stringReadings[0] == "E" && stringReadings[3] == ";")
+                        //{
+                        //intReadings = getIntReadings(stringReadings);
+                        //}
+                        //else
+                        //{
+                        //{
+                        //hard coded to most common error for now
+
+
+                        //      strCopy = stringReadings;
+                        //    stringReadings[0] = strCopy[2][1].ToString();
+                        //  Debug.Print("New index 0 " + stringReadings[0]);
                         //stringReadings[1] = strCopy[3] + strCopy[0];
                         //Debug.Print("New index 1 " + stringReadings[1]);
                         //stringReadings[2] = strCopy[1];
@@ -130,53 +174,17 @@ namespace HERO_Serial_Example4
                         //Debug.Print("New index 3 " + stringReadings[3]);
 
                         //intReadings = getIntReadings(stringReadings);
-                    //}
-
-
-                    while (intReadings[0] < 90)
-                    //while (true)
-                    {
-                        //set motor to run forward until specified angle
-                        //motor0.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, .25);
-
-                        //_uart.Read(_rx, 0, 13);
-                        //Debug.Print("BYTEs TO READ = " + _uart.BytesToRead.ToString());
-                        //System.Threading.Thread.Sleep(10);
-                        if (_rx != null)
-                        {
-                            stringReadings = getStrReadings();
-                        }
-                        //if (stringReadings[0] == "E" && stringReadings[3] == ";")
-                        //{
-                            intReadings = getIntReadings(stringReadings);
                         //}
-                        //else
-                        //{
-                            //{
-                                //hard coded to most common error for now
-
-
-                          //      strCopy = stringReadings;
-                            //    stringReadings[0] = strCopy[2][1].ToString();
-                              //  Debug.Print("New index 0 " + stringReadings[0]);
-                                //stringReadings[1] = strCopy[3] + strCopy[0];
-                                //Debug.Print("New index 1 " + stringReadings[1]);
-                                //stringReadings[2] = strCopy[1];
-                                //Debug.Print("New index 2 " + stringReadings[2]);
-                                //stringReadings[3] = strCopy[2][0].ToString();
-                                //Debug.Print("New index 3 " + stringReadings[3]);
-
-                                //intReadings = getIntReadings(stringReadings);
-                            //}
 
                         //}
 
 
                         Debug.Print("Motor running, goal not met");
 
-                        //CTRE.Phoenix.Watchdog.Feed();
+                        CTRE.Phoenix.Watchdog.Feed();
                     }
                     Debug.Print("Motor stopped, goal met");
+                //}
 
                     /* wait a bit, keep the main loop time constant, this way you can add to this example (motor control for example). */
                     System.Threading.Thread.Sleep(10);
@@ -193,8 +201,57 @@ namespace HERO_Serial_Example4
 
         private static string[] getStrReadings()
         {
-            _uart.Read(_rx, 0, 13);
+            int ndx = 0;
+            char[] charsReadIn;
+            char[] receivedChars = new char[1024];
+            string str;
+            bool recvInProgress = false;
+            newData = false;
+            //_uart.Read(_rx, 0, 1);
+            while(_uart.BytesToRead > 0 && newData == false)
+            {
+                _uart.Read(_rx, 0, 1);
+                charsReadIn = Encoding.UTF8.GetChars(_rx);
+                //receivedChars = new char[charsReadIn.Length];
+                if (recvInProgress == true)
+                {
+                    if (charsReadIn[0] != ';' && charsReadIn[0] != 'E')
+                    {
+                        receivedChars[ndx] = charsReadIn[0];
+                        ndx++;
+                        if(receivedChars[ndx-1] == '-' && charsReadIn[0] == '-')
+                        {
+                            Debug.Print("two hyphens exception");
+                            newData = false;
+                        }
+                        if (ndx >= 1023)
+                        {
+                            ndx = 1023 - 1;
+                        }
+                    }
+                
+                    else
+                     {
+                        if (charsReadIn[0] == 'E')
+                        {
+                            newData = false;
+                        }
+                        else
+                        { 
+                            newData = true;
+                        }
+                        receivedChars[ndx] = '\0';
+                        recvInProgress = false;
+                        ndx = 0;
+                    }
+                }
+                else if (charsReadIn[0] == 'E')
+                {
+                    recvInProgress = true;
+                }
+            } 
             System.Threading.Thread.Sleep(10);
+            /*
             while (_rx == null)
             {
                 Debug.Print("rx is null");
@@ -203,10 +260,13 @@ namespace HERO_Serial_Example4
             }
             Debug.Print("converting to chars");
             char[] readIn = Encoding.UTF8.GetChars(_rx);
-            while (readIn[0] != 'E' || readIn[12] != ';'||readIn.Length<13)
+            while (readIn[0] != 'E' || readIn[readIn.Length-1] != ';'||readIn.Length<13)
             {
                 System.Threading.Thread.Sleep(10);
                 Debug.Print("reordering");
+                //string str = new string(readIn);
+                str = new string(readIn);
+                Debug.Print(str);
                 //for (int n = 0; )
                 _uart.Read(_rx, 0, 13);
                 readIn = Encoding.UTF8.GetChars(_rx);
@@ -256,24 +316,17 @@ namespace HERO_Serial_Example4
             */
             //char[] readIn = Encoding.UTF8.GetChars(_rx);
 
-            string str = new string(readIn);
-            Debug.Print(str);
-            //if(str[0] != 'E')
-            //{
-            //do
-            //{
-            //  char temp = str[0];
-            //for(int m  = 0; m < str.Length - 1)
-            //{
 
-            //                    }
-
-            //              }
-            //            while (str[0] != 'E');
-
-            string[] readings = str.Split('-');
-            //Debug.Print(readings[0]);
-            //Debug.Print(readings[1]);
+            //string str = new string(readIn);
+            //if(receivedChars[0] == '-' && receivedChars[receivedChars.Length-6] == '-' && receivedChars[receivedChars.Length-1] == '-')
+            //{
+                str = new string(receivedChars);
+                Debug.Print(str);
+                string[] readings = str.Split('-');
+            //}
+            //str = new string(receivedChars);
+            //Debug.Print(str);
+            //string[] readings = str.Split('-');
             return readings;
         }    
             
